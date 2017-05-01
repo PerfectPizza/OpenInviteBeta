93496e73eae8d5bbe69fc62ebd6a764499297444
Ross Topol <rtopol2@gmail.com>
2017-05-01 13:46:52 -0500

    adjust for airbnb linting

--- server.js
+++ server.js
@@ -2,23 +2,23 @@ const express = require('express');
 const webpackDevMiddleware = require('webpack-dev-middleware');
 const webpack = require('webpack');
 const webpackConfig = require('./webpack.config.js');
-const app = express();
 
+const app = express();
 const compiler = webpack(webpackConfig);
 
-app.use(express.static(__dirname + '/www'));
+app.use(express.static(`${__dirname}/www`));
 
 app.use(webpackDevMiddleware(compiler, {
   hot: true,
   filename: 'bundle.js',
   publicPath: '/',
   stats: {
-    colors: true
+    colors: true,
   },
-  historyApiFallback: true
+  historyApiFallback: true,
 }));
 
-const server = app.listen(3000, function() {
+const server = app.listen(3000, () => {
   const host = server.address().address;
   const port = server.address().port;
   console.log('App listening at http://%s:%s', host, port);
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const path = require('path');
- 
+
 module.exports = {
   context: path.join(__dirname, 'src'),
   entry: [
