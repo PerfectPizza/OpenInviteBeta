87ad559d6c9d99c1d465754e01da3c3d11a369dc
Ross Topol <rtopol2@gmail.com>
2017-05-01 14:10:47 -0500

    add react

--- src/main.js
+++ src/main.js
@@ -1,1 +1,10 @@
-console.log('Hello World!');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Counter from './Counter';
+
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDOM.render(
+    React.createElement(Counter),
+    document.getElementById('mount'),
+  );
+});
