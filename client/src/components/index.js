4fcc41f848ccc6f668217f581f6eed2dbbf6e3c9
Ari Frankel <ari.l.frankel@gmail.com>
2017-05-25 18:22:18 -0500

    committing changes to components/index. TODO: use it

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,11 +1,13 @@
 import AddEdit from './AddEdit';
 import Event from './Event';
-import List from './List/index.jsx';
+import List from './List';
 import GMap from './GMap';
+import Sidebar from './Sidebar';
 
 export default {
   AddEdit,
   Event,
   List,
   GMap,
+  Sidebar,
 };
