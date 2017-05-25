61cbc7d2a81e1ea5c433e4f2594e4b5a9f6a9ded
Ari Frankel <ari.l.frankel@gmail.com>
2017-05-25 18:21:46 -0500

    sidebar classful component so it can initialize sidenav on didMount

--- client/src/components/Sidebar/index.js
+++ client/src/components/Sidebar/index.js
@@ -1,23 +1,40 @@
-import React from 'react';
+/* global $ */
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 require('./styles.css');
 
-const Sidebar = () => (
-  <ul id="slide-out" className="side-nav fixed">
-    <li><div className="userView">
-      <div className="background"></div>
-      <Link to="/">
-        <img alt="user" className="circle" src="https://media.licdn.com/mpr/mpr/shrinknp_200_200/p/1/005/00c/19b/270823d.jpg" />
-      </Link>
-    </div></li>
-    <hr/>
-    <li><Link to="/"><i className="material-icons large">view_list</i></Link></li>
-    <hr/>
-    <li><Link to="/map"><i className="material-icons large">place</i></Link></li>
-    <hr/>
-    <li><Link to="/edit"><i className="material-icons large">add</i></Link></li>
-  </ul>
-);
+class Sidebar extends Component {
+
+  componentDidMount() {
+    $('.button-collapse').sideNav({
+      menuWidth: 200,
+      draggable: true,
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <ul id="slide-out" className="side-nav fixed">
+          <li><div className="userView">
+            <Link to="/list">
+              <img alt="user" className="circle" src="https://media.licdn.com/mpr/mpr/shrinknp_200_200/p/1/005/00c/19b/270823d.jpg" />
+            </Link>
+          </div></li>
+          <hr />
+          <li><Link to="/list"><i className="material-icons large">view_list</i></Link></li>
+          <hr />
+          <li><Link to="/map"><i className="material-icons large">place</i></Link></li>
+          <hr />
+          <li><Link to="/edit"><i className="material-icons large">add</i></Link></li>
+        </ul>
+        <a data-activates="slide-out" className="button-collapse">
+          <i className="material-icons">menu</i>
+        </a>
+      </div>
+    );
+  }
+}
 
 export default Sidebar;
