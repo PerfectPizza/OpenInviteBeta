e6eee59029ad6ad188d7aa360e6d999ed4d041c7
Ari Frankel <ari.l.frankel@gmail.com>
2017-05-25 18:12:16 -0500

    add devtools middleware. combine reducers

--- client/src/store.js
+++ client/src/store.js
@@ -1,6 +1,11 @@
-import { createStore } from 'redux';
+import { createStore, combineReducers } from 'redux';
 import ListReducers from './components/List/reducers';
 
-export default createStore(
-  ListReducers,
+const combinedReducers = combineReducers({ ListReducers });
+
+const store = createStore(
+  combinedReducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
 );
+
+export default store;
