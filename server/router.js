b4df53642ea043ea37697824d5434497821ecbba
Ari Frankel <ari.l.frankel@gmail.com>
2017-05-24 20:43:58 -0500

    change create event route to be more conventionally RESTful

--- server/router.js
+++ server/router.js
@@ -2,18 +2,18 @@ const router = require('express').Router();
 const { user, event } = require('./controllers');
 
 router.get('/user', user.getAllUsers);
-router.get('/user/:FBID', user.getUser);
-router.get('/user/:FBID/event', event.getEventsByUserId);
+router.get('/user/:_id', user.getUser);
+router.get('/user/:_id/event', event.getEventsByUserId);
 router.get('/event/:event_id', event.getEvent);
 router.get('/event/:event_id/attendee', event.getAttendeesByEventId);
 
 router.post('/user', user.createUser);
-router.post('/user/:FBID/event', event.createEvent);
+router.post('/event', event.createEvent);
 router.post('/event/:event_id/attendee', event.addAttendeeByEventId);
 
 router.put('/event/:event_id', event.updateEvent);
 
 router.delete('/event/:event_id', event.deleteEvent);
-router.delete('/event/:event_id/attendee/:FBID', event.removeAttendee);
+router.delete('/event/:event_id/attendee/:_id', event.removeAttendee);
 
 module.exports = router;
