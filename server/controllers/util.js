9c2485c54d13c55663ee65d27cd43dfb2901f01d
Ari Frankel <ari.l.frankel@gmail.com>
2017-05-24 20:56:56 -0500

    comments

--- server/controllers/util.js
+++ server/controllers/util.js
@@ -1,4 +1,5 @@
 module.exports = {
+  /* decompose the Mongo error object in to an array of error messages */
   parseErr(err) {
     if (!err.errors) return err;
     const messages = Object.keys(err.errors).map(errorKey =>
