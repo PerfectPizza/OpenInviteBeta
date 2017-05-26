258a46ae510dfd8d10e7c6b05eb1dfb1b53c785f
Ross Topol <rtopol2@gmail.com>
2017-05-26 10:53:31 -0500

    implement FB login first stage

---
+++ client/src/views/index.js
@@ -0,0 +1,9 @@
+import EventEditView from './EventEditView';
+import ListView from './ListView';
+import EventFullView from './EventFullView';
+
+export default {
+  EventEditView,
+  ListView,
+  EventFullView,
+};
