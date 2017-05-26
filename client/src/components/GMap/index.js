4b2f83719048ffa7acc9c2a5a6dc9e2b3f4e5dc7
Ari Frankel <ari.l.frankel@gmail.com>
2017-05-25 20:26:13 -0500

    GMap stub

--- client/src/components/GMap/index.js
+++ client/src/components/GMap/index.js
@@ -1,4 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
+import { withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { addEvents } from '../actions/events';
 
-export default () => (<div>MapView</div>)
+require('./styles.css');
 
+class GMap extends Component {
+  constructor({ user }) {
+    super();
+    this.state = {
+      events: [],
+    };
+    this.user_id = user ? user._id : '987';
+  }
+
+  componentDidMount() {
+    axios.get(`/api/user/${this.user_id}/event`)
+      .then(({ data: events }) => {
+        this.props.addEvents(events);
+        this.setState({ events });
+      });
+  }
+
+  render() {
+    return (
+      <div>
+        <ul className="collection">
+          {this.state.events && this.state.events.map(event => <li>{event.title}</li>)}
+        </ul>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = ({ user }) => ({ user });
+
+const mapDispatchToProps = dispatch => ({
+  addEvents: (events) => {
+    dispatch(addEvents(events));
+  },
+});
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(GMap));
