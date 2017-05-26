210bba5f1f78c0a48da5345abc0a3519711c7c3a
Ross Topol <rtopol2@gmail.com>
2017-05-26 11:14:50 -0500

    delete views folder

--- client/src/views/index.js
+++
@@ -1,9 +0,0 @@
-import EventEditView from './EventEditView';
-import ListView from './ListView';
-import EventFullView from './EventFullView';
-
-export default {
-  EventEditView,
-  ListView,
-  EventFullView,
-};
