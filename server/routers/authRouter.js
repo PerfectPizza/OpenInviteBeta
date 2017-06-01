30e1b59b5b1533fde80d979e3e5274d6908a904e
Ari Frankel <ari.l.frankel@gmail.com>
2017-05-31 23:23:53 -0500

    rm incorrect comment

--- server/routers/authRouter.js
+++ server/routers/authRouter.js
@@ -1,6 +1,5 @@
 const router = require('express').Router();
 const passport = require('passport');
-
 router.get('/facebook', passport.authenticate('facebook', { scope: 'user_friends' }));
 
 router.get('/facebook/return',
