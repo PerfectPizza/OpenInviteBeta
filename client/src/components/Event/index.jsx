57d1eb32dd429bed8eb079867ea99d0bc51d01eb
Ari Frankel <ari.l.frankel@gmail.com>
2017-06-03 18:44:11 -0500

    flush out event view

--- client/src/components/Event/index.jsx
+++ client/src/components/Event/index.jsx
@@ -2,25 +2,52 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { withRouter, Link } from 'react-router-dom';
 import { connect } from 'react-redux';
+import axios from 'axios';
+import { addAttendee, removeAttendee } from '../actions/events';
+import RSVP from '../RSVP';
 
-const Event = ({ event, user }) => {
-  console.log(event);
+const Event = ({ event, user, events }) => {
   const { title, start_time: startTime, end_time: endTime, attendees } = event;
   return (
-    <div className="main">
-      <h2>{title}</h2>
-      {user._id === event.creator &&
-        <Link to={`/edit/${event._id}`}>
-          <span className="btn-floating btn-large waves-effect waves-light right">
-            <i className="right material-icons small blue">
-              mode_edit
-            </i>
-          </span>
-        </Link>}
-      <ul>
-        <li>{startTime}</li>
-        <li>{endTime}</li>
-      </ul>
+    <div>
+      <div className="row">
+        <div className="col s8">
+          <div className="card">
+            <div className="card-content">
+              {user._id === event.creator &&
+              <Link to={`/edit/${event._id}`}>
+                <span className="btn-floating btn-large waves-effect waves-light right">
+                  <i className="right material-icons small blue">
+                    mode_edit
+                  </i>
+                </span>
+              </Link>}
+              <h3 className="center">{title}</h3>
+              <div>MAP</div>
+            </div>
+          </div>
+        </div>
+        <div className="col s3">
+          <div className="card">
+            <span className="card-title">Attendees</span>
+            <div className="card-action">
+              {event.attendees.length ?
+                event.attendees.map(attendee =>
+                  <span key={attendee._id}>
+                    {attendee.name}
+                  </span>
+                ) : 
+                  <span key="0">
+                    Be the first to RSVP
+                  </span>
+              }
+              <div className="card-action">
+                <RSVP event={event}/>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
@@ -41,6 +68,16 @@ Event.propTypes = {
 const mapStateToProps = ({ events, user }, { match }) => ({
   event: events.find(event => event._id === match.params.event_id),
   user,
+  events
 });
 
-export default withRouter(connect(mapStateToProps)(Event));
+const mapDispatchToProps = dispatch => ({
+  addAttendee: (event_id, user_id, userName) => {
+    dispatch(addAttendee(event_id, user_id, userName));
+  },
+  removeAttendee: (event_id, user_id) => {
+    dispatch(removeAttendee(event_id, user_id));
+  },
+});
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Event));
